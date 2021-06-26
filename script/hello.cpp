d0d2f1bad1b6ebc57a30977ea22607da20730a1c
kennydukor <kennydukor@gmail.com>
2021-06-25 22:33:23 -0700

    First Commit

---
+++ script/hello.cpp
@@ -0,0 +1,23 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+	//inherit from WorldPlugin class
+	class WorldPluginWheeledRobot: public WorldPlugin
+	{
+		public: WorldPluginWheeledRobot(): WorldPlugin()
+		{
+			printf("Hello Instructor!\n");
+		}		
+		
+		// This is mandatory to recieve information from the world file
+		public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+		{
+		
+		}
+	};
+	GZ_REGISTER_WORLD_PLUGIN(WorldPluginWheeledRobot)
+}
+
+// Note: this should be build with cmake
+// Dont forget to export this -> export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/home/robond/Documents/Robot_Project/build
